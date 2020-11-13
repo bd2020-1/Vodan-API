22b59c9d66b5ec07228cf5b020a8d2c15f44cdce
Rodrigo Passos <rodrigo.passos@visagio.com>
2020-11-12 21:57:17 -0300

    [UPDATE]Ordenando por modulo e extraindo como date ao inves de datetime

--- src/sql/select/get_all_modules_per_participant.sql
+++ src/sql/select/get_all_modules_per_participant.sql
@@ -1,4 +1,5 @@
-SELECT formRecordID, dtRegistroForm, crfFormsID FROM tb_formrecord c
+SELECT formRecordID, CAST(dtRegistroForm as DATE) as dtRegistroForm , crfFormsID FROM tb_formrecord c
 LEFT join tb_assessmentquestionnaire b 
 ON b.participantID = c.participantID
-where b.participantID ={participant_id};
+where b.participantID ={participant_id}
+order by crfFormsID, dtRegistroForm;
