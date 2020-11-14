708003409ab83daa0b661663dcddb9350ffca6f6
Rodrigo Passos <rodrigo.passos@visagio.com>
2020-11-13 23:45:58 -0300

    [UPDATE]Deixando apenas o que corresponde a endpoints dos modulos

--- src/routes/modules.py
+++ src/routes/modules.py
@@ -4,8 +4,6 @@ from fastapi import APIRouter, status, FastAPI
 
 from config import database
 from models.questions import Question
-from models.modules import ParticipantModuleAnswer, ParticipantModuleGroupAnswer, ParticipantModules
-from models.modules import ParticipantModuleAnswer, ParticipantModuleGroupAnswer, ParticipantModuleDate
 from utils import get_sql_file
 
 
@@ -36,72 +34,5 @@ async def get_all_questions_from_module(module_id: int):
             question["ListValues"] = values
         questions[idx] = question
     return questions
-
-
-@router.get(
-    "/{module_id}/participants/{participant_id}",
-    response_model=List[ParticipantModuleAnswer],
-    status_code=status.HTTP_200_OK,
-)
-async def get_all_answers_from_module_per_participant(
-    module_id: int, participant_id: int
-):
-    _query = get_sql_file(file_path_name="select/get_module_per_participant").format(
-        module_id=module_id, participant_id=participant_id
-    )
-
-    groups = await database.fetch_all(_query)
-
-    return groups
-
-@router.get(
-    "/{module_id}/questions/{participant_id}",
-    response_model=List[ParticipantModuleDate],
-    status_code=status.HTTP_200_OK,
-)
-async def get_all_answers_from_module_per_participant_per_date(
-    module_id: int, participant_id: int, data_attendance: str = None
-):
-    _query = get_sql_file(file_path_name="select/get_answers_per_participant_per_date").format(
-        module_id=module_id, participant_id=participant_id, data_attendance=data_attendance
-    )
-
-    groups = await database.fetch_all(_query)
-
-    return groups
-
-
-@router.get(
-    "/participants/{participant_id}/modules",
-    response_model=List[ParticipantModules],
-    status_code=status.HTTP_200_OK,
-)
-async def get_all_modules_from_participant(
-    participant_id: int
-):
-    _query = get_sql_file(
-        file_path_name="select/get_all_modules_per_participant"
-    ).format(participant_id=participant_id)
-
-    groups = await database.fetch_all(_query)
-
-    return groups
-
-
-@router.get(
-    "/{module_id}/groups/{group_id}/participants/{participant_id}",
-    response_model=List[ParticipantModuleGroupAnswer],
-    status_code=status.HTTP_200_OK,
-)
-async def get_all_answers_from_module_group_per_participant(
-    module_id: int, group_id: int, participant_id: int
-):
-    _query = get_sql_file(
-        file_path_name="select/get_module_group_per_participant"
-    ).format(module_id=module_id, group_id=group_id, participant_id=participant_id)
-
-    groups = await database.fetch_all(_query)
-
-    return groups
 
 
