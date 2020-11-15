0aa7652f34a95c504686bd0d444ac8c2dc65d629
gabriellydeandrade <gabrielly.andrade@poli.ufrj.br>
2020-11-15 16:35:15 -0300

    fix: get question groups in order

--- src/sql/select/get_all_questiongroups_from_module.sql
+++ src/sql/select/get_all_questiongroups_from_module.sql
@@ -1,8 +1,12 @@
-SELECT *
-FROM tb_questiongroup
-WHERE questionGroupID IN (
-    SELECT questionGroupID
-    FROM tb_questiongroupform AS PV INNER JOIN tb_questions AS Q
-    ON PV.crfFormsID = {module_id} AND PV.questionID = Q.questionID
-    GROUP BY questionGroupID
-)
+SELECT q.questionGroupID, q.description, q.comment
+FROM tb_questiongroup AS q
+INNER JOIN (
+	SELECT questionGroupID, questionOrder
+	FROM tb_questiongroupform AS PV
+	INNER JOIN tb_questions AS Q
+		ON PV.questionID = Q.questionID
+		AND PV.crfFormsID = {module_id}
+) AS form
+	ON q.questionGroupID = form.questionGroupID
+GROUP BY q.questionGroupID, q.description, q.comment
+ORDER BY MIN(form.questionOrder)
