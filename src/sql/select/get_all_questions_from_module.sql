4b15230a407f3e36ff30646c168a2330d95711b0
Rodrigo Passos <rodrigo.passos@visagio.com>
2020-11-14 23:24:33 -0300

    [UPDATE]Ordenando a consulta na requisição get_all_questions_from_module

--- src/sql/select/get_all_questions_from_module.sql
+++ src/sql/select/get_all_questions_from_module.sql
@@ -1,3 +1,4 @@
 SELECT crfFormsID, PV.questionID, questionOrder, description, questionTypeID, listTypeID, questionGroupID, subordinateTo, isAbout
 FROM tb_questiongroupform AS PV INNER JOIN tb_questions AS Q
 ON PV.crfFormsID = {module_id} AND PV.questionID = Q.questionID
+ORDER BY questionOrder;
